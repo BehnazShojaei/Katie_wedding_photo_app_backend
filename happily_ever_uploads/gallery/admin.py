8442f6fd66e868eaf34f1814ec8fa59e77006ebe
Gina <168549659+GinaHorch@users.noreply.github.com>
2025-02-26 17:23:12 +0800

    changes to admin, models and views to get the test-upload principle working via admin

--- happily_ever_uploads/gallery/admin.py
+++ happily_ever_uploads/gallery/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Image
 
-admin.site.register(Image)
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'caption', 'uploaded_by', 'uploaded_at', 'get_image_url')
+    list_filter = ('uploaded_at', 'uploaded_by')
+    search_fields = ('caption', 'uploaded_by__username')
+    readonly_fields = ('uploaded_at', 'get_image_url')
+
+    def get_image_url(self, obj):
+        return obj.image.url if obj.image else ''
+    get_image_url.short_description = 'Image URL'
+
+    def save_model(self, request, obj, form, change):
+        if not obj.uploaded_by_id:
+            obj.uploaded_by = request.user
+        super().save_model(request, obj, form, change)
--- happily_ever_uploads/gallery/models.py
+++ happily_ever_uploads/gallery/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
 
 class Image(models.Model):
-    name = models.CharField(max_length=255)  
-    image = models.ImageField(upload_to='media/')  
-    comment = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='media/', storage=S3Boto3Storage())
+    caption = models.CharField(max_length=200, blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    uploaded_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='uploaded_images'
+    )
 
     def __str__(self):
-        return f"Image by {self.name}"
+        return f"Image uploaded by {self.uploaded_by} at {self.uploaded_at}"
