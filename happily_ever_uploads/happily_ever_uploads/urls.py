e4702e51ec96eee7a6554293d351cb076ec86257
Behnaz <behnaz.shojaei88@gmail.com>
2025-02-26 23:39:28 +0800

    keep gallery urls at base root

--- happily_ever_uploads/happily_ever_uploads/urls.py
+++ happily_ever_uploads/happily_ever_uploads/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('gallery.urls')),
+    # path('api/', include('gallery.urls')),
     path('', include('users.urls')),
     path('', include('gallery.urls')),
 
