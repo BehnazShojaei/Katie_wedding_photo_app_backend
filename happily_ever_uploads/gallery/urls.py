feb26958d460f51a610eb68a3b97cf112030bb77
Behnaz <behnaz.shojaei88@gmail.com>
2025-02-26 15:19:36 +0800

    added endpoint for each image

--- happily_ever_uploads/gallery/urls.py
+++ happily_ever_uploads/gallery/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ImageListCreateView, ImageDeleteView
+from .views import ImageListCreateView, ImageDeleteView, ImageDetailView
 from . import views
 
 urlpatterns = [
     path('images/', ImageListCreateView.as_view(), name='image-list-create'),  
+    path('images/<int:pk>/', ImageDetailView.as_view(), name='image-delete'), 
     path('images/<int:pk>/delete/', ImageDeleteView.as_view(), name='image-delete'), 
     path('test-upload/', views.test_upload, name='test-upload'),
 ]
