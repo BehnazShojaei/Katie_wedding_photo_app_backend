e367124f8839eacc367459b024b15b8f7b6536a9
Behnaz <behnaz.shojaei88@gmail.com>
2025-02-28 13:25:54 +0800

    rename the delete endpoint

--- happily_ever_uploads/gallery/urls.py
+++ happily_ever_uploads/gallery/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('images/', ImageListCreateView.as_view(), name='image-list-create'),  
     path('images/<int:pk>/', ImageDetailView.as_view(), name='image-delete'), 
-    path('images/<int:pk>/delete/', ImageDeleteView.as_view(), name='image-delete'), 
+    path('images/delete/<int:pk>/', ImageDeleteView.as_view(), name='image-delete'), 
     path('test-upload/', views.test_upload, name='test-upload'),
 ]
