670ff0e0562150b983cea58c1c827443c3a06478
Behnaz <behnaz.shojaei88@gmail.com>
2025-03-15 15:51:57 +0800

    updated serializer, urls and views to enable admin username update

--- happily_ever_uploads/users/serializers.py
+++ happily_ever_uploads/users/serializers.py
@@ -25,20 +25,27 @@ class CustomUserSerializer(serializers.ModelSerializer):
             instance.set_password(validated_data.pop('password'))  # Hash password properly
             for attr, value in validated_data.items():
                 setattr(instance, attr, value)
-                instance.save()  # Explicitly save the updated instance
+                instance.save()  
                 return instance
 
-class ChangeAdminPasswordSerializer(serializers.Serializer):
-    old_password = serializers.CharField(write_only=True)
-    new_password = serializers.CharField(write_only=True)
+class ChangeAdminSerializer(serializers.Serializer):
+    current_password = serializers.CharField(write_only=True)
+    new_password = serializers.CharField(write_only=True, required=False)
+    new_username = serializers.CharField(required=False)
 
-    def validate_old_password(self, value):
+    def validate_current_password(self, value):
         user = self.context['request'].user
         if not user.check_password(value):
-            raise serializers.ValidationError("Old password is incorrect")
+            raise serializers.ValidationError("Current password is incorrect")
         return value
 
     def update(self, instance, validated_data):
-        instance.set_password(validated_data['new_password'])  # Hash new password
+        if 'new_password' in validated_data:
+            instance.set_password(validated_data['new_password'])  
+        
+        if 'new_username' in validated_data:
+            instance.username = validated_data['new_username']  
+            
+        
         instance.save()
         return instance
--- happily_ever_uploads/users/urls.py
+++ happily_ever_uploads/users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import LoginView, UpdateGuestUserView, ChangeAdminPasswordView
+from .views import LoginView, UpdateGuestUserView, ChangeAdminCredentialsView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),  # Single login for both admin & guest
     path('update-guest/', UpdateGuestUserView.as_view(), name='update-guest'),  # Admin updates guest credentials
-    path('change-password/', ChangeAdminPasswordView.as_view(), name='change-password'),  # Admin changes their password
+    path('change-credentials/', ChangeAdminCredentialsView.as_view(), name='change-password'),  # Admin changes their credentials
 
 ]
